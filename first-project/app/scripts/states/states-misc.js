b4f66fb83fa77efcc2fa5973f94e9f444711328e
apple <apple@apples-MacBook-Pro.local>
2015-08-17 21:56:40 +0530

    firstcommit

---
+++ first-project/app/scripts/states/states-misc.js
@@ -0,0 +1,10 @@
+/*'use strict';
+
+angular.module('app.states').config($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider.state('app.main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      }));
+
+}*/
